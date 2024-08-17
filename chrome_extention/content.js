60930eed39ee5d2eb64c9ef79713ea73adc437ff
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2024-08-17 12:13:33 +0900

    class名違った

--- chrome_extention/content.js
+++ chrome_extention/content.js
@@ -1,4 +1,14 @@
 /*
 曲のclass => no-overflow
 スコアのclass => score
+
+スコアのクラス名違ったわ
 */
+
+let music = document.querySelectorAll(".no-overflow")
+let score = document.querySelectorAll('#experience div')
+let button = document.createElement('button')
+button.addEventListener('click',function(){
+
+})
+
