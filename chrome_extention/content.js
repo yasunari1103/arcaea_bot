0bc2b20febbf5a95f45050d12a7e830a38bf3f0d
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2024-08-19 13:44:01 +0900

    作業する場所をノートからデスクトップに変えるよ

--- chrome_extention/content.js
+++ chrome_extention/content.js
@@ -1,14 +1,22 @@
 /*
 曲のclass => no-overflow
-スコアのclass => score
-
-スコアのクラス名違ったわ
+スコアのclass => ex-main EX+
 */
 
-let music = document.querySelectorAll(".no-overflow")
-let score = document.querySelectorAll('#experience div')
-let button = document.createElement('button')
+let score = document.getElementsByClassName('card');
+let musicName = score.getElementsByClassName('')
+musicList = [];
+for (let i = 1;i < 60;i += 2) {musicList.push(score[i])};
+
+
+let button = document.createElement('button');
 button.addEventListener('click',function(){
 
 })
 
+/*
+let score = document.getElementsByClassName('card');
+for (let i = 1;i < 60;i += 2) {music_list.push(score[i])}
+music_list[0]
+
+*/
