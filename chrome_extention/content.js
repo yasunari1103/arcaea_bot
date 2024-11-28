9748608b39da54893f2b44b602522d0dfd2dab72
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-11-28 16:12:36 +0900

    ちょっと進んだ

--- chrome_extention/content.js
+++ chrome_extention/content.js
@@ -51,36 +51,57 @@ getJSONButton.style.fontFamily =
   "Open Sans,Yu Gothic Medium,Yu Gothic,Noto Sans JP,sans-serif";
 document.body.prepend(getJSONButton);
 
-
 //////////////////////////////////////////////////////////////////////////////////////////
 
-let checkBestPotentialButton = document.createElement('button');
-checkBestPotentialButton.addEventListener('click', function () {
-  musicList = {};
-  let result = document.getElementsByClassName("card");
-
-  for (let i = 1; i < 60; i += 2) {
-    let musicName =
-      result[i].getElementsByClassName("no-overflow")[0].textContent;
-    let musicScore = parseInt(
-      result[i]
-        .getElementsByClassName("experince")[0]
-        .textContent.replace(/[^0-9]/g, ""),
-      10
-    );
-    difficultyLevel =
-      result[i].getElementsByClassName("label small-label")[0].textContent;
-    musicList[(i/2+0.5) + '. ' + musicName + "[" + difficultyLevel + "]"] = musicScore;
-  }
-  console.log(musicList);
-})
+let checkBestPotentialButton = document.createElement("button");
+checkBestPotentialButton.addEventListener("click", function () {
+  const jsonPath = chrome.runtime.getURL("musicList.json"); //同梱されているJSONファイルの読み込み
+  fetch(jsonPath)
+    .then((response) => response.json())
+    .then((data) => {
+      let bestMusicList = [];
+      let result = document.getElementsByClassName("card");
+      for (let i = 1; i < 60; i += 2) {
+        let musicName =
+          result[i].getElementsByClassName("no-overflow")[0].textContent;
+        let musicScore = parseInt(
+          result[i]
+            .getElementsByClassName("experince")[0]
+            .textContent.replace(/[^0-9]/g, ""),
+          10
+        );
+        let difficultyLevel =
+          result[i].getElementsByClassName("label small-label")[0].textContent;
+        bestMusicList.push({
+          number: i / 2 + 0.5,
+          name: musicName + "[" + difficultyLevel + "]",
+          score: musicScore,
+        });
+      }
+      console.log(bestMusicList);
+      bestMusicList.forEach((element) => {
+        Object.entries(data).forEach(([key, value]) => {
+          if (element.name === key) {
+            /////ここから修正
+            let Const = value;
+            let Score = element.score;
+            console.log(element.number, key, value, element.score);
+          }
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error loading JSON:", error);
+    });
+});
 checkBestPotentialButton.textContent = "ベスト枠を確認！";
 checkBestPotentialButton.className = "button";
 
 //ボタンのcss
 checkBestPotentialButton.style.width = "100%";
 checkBestPotentialButton.style.height = "auto";
-checkBestPotentialButton.style.background = "linear-gradient(to right, #B0FFFF, pink)";
+checkBestPotentialButton.style.background =
+  "linear-gradient(to right, #B0FFFF, pink)";
 checkBestPotentialButton.style.fontFamily =
   "Open Sans,Yu Gothic Medium,Yu Gothic,Noto Sans JP,sans-serif";
 document.body.prepend(checkBestPotentialButton);
