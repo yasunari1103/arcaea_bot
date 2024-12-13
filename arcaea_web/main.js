847a0e5c5b0a9d2867294acf1506da08a3ecd714
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2024-12-14 05:08:58 +0900

    input作成

---
+++ arcaea_web/main.js
@@ -0,0 +1,5 @@
+function randonMusicByConst() {
+    let musicConstMin = document.getElementById("const_min").value;
+    let musicConstMax = document.getElementById("const_max").value;
+    console.log(musicConstMin,musicConstMax)
+}
