3935c744a5e643d2db8e13dd8c925817080e24a3
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-12-16 15:22:17 +0900

    簡易サーバー立ち上げて実行してください

--- arcaea_web/main.js
+++ arcaea_web/main.js
@@ -1,5 +1,21 @@
 function randonMusicByConst() {
-    let musicConstMin = document.getElementById("const_min").value;
-    let musicConstMax = document.getElementById("const_max").value;
-    console.log(musicConstMin,musicConstMax)
+  let musicConstMin = document.getElementById("const_min").value;
+  let musicConstMax = document.getElementById("const_max").value;
+  fetch("musicList.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTPS error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(musicConstMax);
+      console.log(musicConstMin);
+      //forで配列作成    <= あとでやる
+      console.log(data);
+      //forEachで配列から取り出して実行    <= あとでやる
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
